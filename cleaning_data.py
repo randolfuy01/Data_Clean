73a577bc18d9a8e61d385d79e31bfadd132ff7d5
Randolf Uy <920265148@sfsu.edu>
2023-09-19 15:33:44 -0700

    Finalized tokenization and removing stop words

--- cleaning_data.py
+++ cleaning_data.py
@@ -1,17 +1,25 @@
-import numpy as np
 import pandas as pd
-import string
-import re
-
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 lyrical_data = pd.read_csv('drake_data.csv')
 
 # Dropping the items as we do not need them within the dataframe
 to_drop = ['album', 'lyrics_url', 'track_views']
 lyrical_data.drop(labels=to_drop, axis=1, inplace=True)
 # Now we have dataframe consisting of just names of the songs and the lyrics
-
-# Cleaning the lyrics data
 lyrical_data['lyrics'] = lyrical_data['lyrics'].str.lower()
-lyrical_data['lyrics'] = lyrical_data['lyrics'].str.replace(r'[^a-zA-Z0-9 ]', ' ', regex=True)
-lyrical_data.to_csv('drake_data_cleaned.csv')
 
+lyrical_data['lyrics'] = lyrical_data['lyrics'].str.replace(r'[^a-zA-Z0-9 ]', ' ', regex = True)
+
+lyrical_data['lyrics'] = lyrical_data['lyrics'].replace(r'\s+', ' ', regex=True)
+
+lyrical_data['lyrics'].to_csv('before_tokenization.csv')
+
+stop = stopwords.words('english')
+
+lyrical_data['lyrics'] = lyrical_data.apply( lambda x : nltk.word_tokenize(str(x['lyrics'])), axis = 1)
+
+lyrical_data['lyrics'] = lyrical_data['lyrics'].apply(lambda x: [item for item in x if item not in stop])
+
+lyrical_data['lyrics'].to_csv('after_tokenization.csv')
