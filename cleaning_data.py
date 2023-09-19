2dd4685c87dab4c3d24df01d4c1965e1d2e9b085
Randolf Uy <920265148@sfsu.edu>
2023-09-19 12:47:25 -0700

    cleaned drake lyrics -> punctuation, special characters

---
+++ cleaning_data.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import string
+import re
+
+lyrical_data = pd.read_csv('drake_data.csv')
+
+# Dropping the items as we do not need them within the dataframe
+to_drop = ['album', 'lyrics_url', 'track_views']
+lyrical_data.drop(labels=to_drop, axis=1, inplace=True)
+print(lyrical_data)
+
+# Now we have dataframe consisting of just names of the songs and the lyrics
+lyrical_data['lyrics'] = lyrical_data['lyrics'].str.lower()
+lyrical_data['lyrics'] = lyrical_data['lyrics'].str.replace(r'[^a-zA-Z0-9 ]', ' ', regex=True)
+print(lyrical_data)
+lyrical_data.to_csv('drake_data_cleaned.csv')
+
